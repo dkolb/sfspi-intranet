9fc2b1546e4685eb602b43ef343524e5708c6e5e
David Kolb <david.kolb@krinchan.com>
2018-12-13 16:03:36 -0500

    Fix tenant urls

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :microsoft_v2_auth, ENV['OFFICE_365_OAUTH_CLIENT'], ENV['OFFICE_365_OAUTH_SECRET']
+  provider :microsoft_v2_auth,
+    ENV['OFFICE_365_OAUTH_CLIENT'],
+    ENV['OFFICE_365_OAUTH_SECRET'],
+    setup: lambda { |env|
+      tenant = ENV['OFFICE_365_OAUTH_TENANT']
+      env['omniauth.strategy'].options[:client_options] = {
+        site: 'https://login.microsoftonline.com',
+        authorize_url: "#{tenant}/oauth2/v2.0/authorize",
+        token_url: "#{tenant}/oauth2/v2.0/token"
+      }
+    }
 end
